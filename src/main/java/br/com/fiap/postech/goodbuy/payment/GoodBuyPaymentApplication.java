aee1adb0d86e6bac8e80c6d432066536748205bb
Anderson0Wagner <anderson.wagner@gmail.com>
2024-06-18 19:15:49 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/postech/goodbuy/payment/GoodBuyPaymentApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.goodbuy.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBuyPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodBuyPaymentApplication.class, args);
+	}
+
+}
